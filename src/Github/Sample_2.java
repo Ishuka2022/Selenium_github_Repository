1bbe22b713b572f783f24710bfd9fdd43b7954b1
igods <igods@DESKTOP-6I0K38Q>
2022-12-04 11:55:23 +0530

    [Ishuka]: Sample_2 is created

---
+++ src/Github/Sample_2.java
@@ -0,0 +1,12 @@
+package Github;
+
+public class Sample_2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Say Hii Aaradhya");
+		System.out.println("plzzz talk with me");
+		System.out.println("Aaradhya Said : talk to my nakhoon");
+	}
+
+}
